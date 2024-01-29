da6f24c8b087a20ee152dc9f44ecd2a431317aa4
valeriano alexander <valerianoalexander@gmail.com>
2024-01-29 08:28:54 +0100

    01. First run test in gherkin

--- cypress/e2e/newtours/newtours.js
+++ cypress/e2e/newtours/newtours.js
@@ -8,6 +8,6 @@ When("action", () => {
   cy.title().should("eq", "Welcome: Mercury Tours");
 });
 
-Then("testable outcome", ()=>{
-    cy.title().should('eq', 'Welcome Mercury Tours')
-})
+Then("testable outcome", () => {
+  cy.title().should("eq", "Welcome: Mercury Tours");
+});
